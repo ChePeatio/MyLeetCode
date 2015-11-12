00027337f9899322748bd37daaf3b51850e6b837
Chepeatio <chepeatio@hotmail.com>
2015-11-12 21:56:21 +0800

    可以了吗

--- src/com/chepeatio/WordLadder/WordLadder1.java
+++ src/com/chepeatio/WordLadder/WordLadder1.java
@@ -1,15 +1,14 @@
-package com.chepeatio.W1ordLadder;
+package com.chepeatio.wordLadder;
 
 import java.util.Set;
 
 /**
  * Created by Che Peatio on 2015/11/12.
  */
-public class WordLadder {
+public class WordLadder1 {
     public int ladderLength(String beginWord, String endWord, Set<String> wordList) {
         int result = 0;
-        for (int i=0; i<1; i++)
-            System.out.println("hehe");
+
         return 0;
     }
 }
--- src/test/com/chepeatio/wordLadder/WordLadderTest.java
+++ src/test/com/chepeatio/wordLadder/WordLadderTest.java
@@ -1,6 +1,6 @@
 package test.com.chepeatio.wordLadder;
 
-import com.chepeatio.W1ordLadder.WordLadder;
+import com.chepeatio.wordLadder.WordLadder;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
