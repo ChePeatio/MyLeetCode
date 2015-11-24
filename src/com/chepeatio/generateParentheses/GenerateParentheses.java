d26403e9149e20c6d6f86cd5f6707e38018b4f2c
chepeatio <chepeatio@hotmail.com>
2015-11-24 21:42:27 +0800

    没能完成这个问题，回宿舍再解决

---
+++ src/com/chepeatio/generateParentheses/GenerateParentheses.java
@@ -0,0 +1,15 @@
+package com.chepeatio.generateParentheses;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Che Peatio on 2015/11/24.
+ */
+public class GenerateParentheses {
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+
+        return res;
+    }
+}
