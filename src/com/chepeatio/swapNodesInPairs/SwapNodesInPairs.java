7bbbea61615b6be154960649d8abd42a3c0dbe6d
Chepeatio <chepeatio@hotmail.com>
2015-12-03 18:59:22 +0800

    今天的题目比较简单，主要关于链表的操作，只要思路清晰、效率高就可以了

--- src/com/chepeatio/swapNodesInPairs/SwapNodesInPairs.java
+++ src/com/chepeatio/swapNodesInPairs/SwapNodesInPairs.java
@@ -7,7 +7,15 @@ public class SwapNodesInPairs {
     public ListNode swapPairs(ListNode head) {
         ListNode res = new ListNode(-1);
         res.next = head;
-
+        ListNode ln1, temp;
+        ln1 = res;
+        while (ln1.next!=null && ln1.next.next!=null) {
+            temp = ln1.next;
+            ln1.next = ln1.next.next;
+            temp.next = ln1.next.next;
+            ln1.next.next = temp;
+            ln1 = temp;
+        }
         return res.next;
     }
 }
