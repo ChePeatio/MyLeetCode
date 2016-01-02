0185aef90c3241246cfd7a2f28fe8bb7ad067083
chepeatio <chepeatio@hotmail.com>
2016-01-02 22:00:02 +0800

    一道Hard题目，做了一晚上还是没做出来，主要是自己分心干别的了，还需要用心来做才行。

---
+++ src/com/chepeatio/burstBalloons/BurstBalloons.java
@@ -0,0 +1,73 @@
+package com.chepeatio.burstBalloons;
+
+/**
+ * Created by Che Peatio on 2016/1/2.
+ */
+public class BurstBalloons {
+    public int maxCoins(int[] nums) {
+        // Judge if there is 0 in the list or the length of the array is 0.
+        for (int num : nums) {
+            if (num==0)
+                return 0;
+        }
+        if (nums.length == 0)
+            return 0;
+
+        // Generate a linked list to store the nums.
+        LinkedNode head = new LinkedNode(-1);
+        LinkedNode tail = new LinkedNode(-1);
+        LinkedNode temp = head, temp2;
+        for (int num : nums) {
+            temp2 = new LinkedNode(num);
+            temp.right = temp2;
+            temp2.left = temp;
+            temp = temp2;
+        }
+        temp.right = tail;
+        tail.left = temp;
+
+        temp = head.right;
+        int res = 1;
+        while (temp.left.val!=-1 || temp.right.val!=-1) {
+            if (temp.right.val > temp.val) {
+                if (temp.left.val == -1) {
+                    res += temp.val * temp.right.val;
+                    temp.right.left = temp.left;
+                    temp.left.right = temp.right;
+                    temp = temp.right;
+                } else {
+                    res += temp.left.val * temp.val * temp.right.val;
+                    temp.right.left = temp.left;
+                    temp.left.right = temp.right;
+                    temp = temp.left;
+                }
+            } else if (temp.right.val == temp.val) {
+                if (temp.left.val == temp.val) {
+                    res += temp.left.val * temp.val * temp.right.val;
+                    temp.right.left = temp.left;
+                    temp.left.right = temp.right;
+                    temp = temp.left;
+                } // there should be more handle process
+            } else if (temp.right.val == -1) {
+                if (temp.left.val > temp.val) {
+
+                }
+                temp = head.right;
+            } else {
+                temp = temp.right;
+            }
+        }
+        res += temp.val;
+        return res;
+    }
+
+    class LinkedNode {
+        LinkedNode left;
+        LinkedNode right;
+        int val;
+
+        public LinkedNode(int num) {
+            val = num;
+        }
+    }
+}
---
+++ src/test/com/chepeatio/burstBalloons/BurstBalloonsTest.java
@@ -0,0 +1,48 @@
+package test.com.chepeatio.burstBalloons;
+
+import com.chepeatio.burstBalloons.BurstBalloons;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+/**
+ * BurstBalloons Tester.
+ *
+ * @author <Authors name>
+ * @version 1.0
+ * @since <pre>Ò»ÔÂ 2, 2016</pre>
+ */
+@RunWith(Parameterized.class)
+public class BurstBalloonsTest {
+
+    BurstBalloons test = new BurstBalloons();
+    int[] param;
+    int res;
+
+    @Parameterized.Parameters
+    public static Collection data() {
+        return Arrays.asList(new Object[][]{
+                {new int[]{1,0,1}, 0},
+                {new int[]{3,1,5,8}, 167},
+                {new int[]{1,1,1,1,1,1,1,1,1}, 1},
+                {new int[]{1,2,3,4,5,6}, 76}
+        });
+    }
+
+    public BurstBalloonsTest(int[] parameter, int result) {
+        param = parameter;
+        res = result;
+    }
+
+    /**
+     * Method: maxCoins(int[] nums)
+     */
+    @Test
+    public void testMaxCoins() throws Exception {
+        Assert.assertEquals(res, test.maxCoins(param));
+    }
+} 
