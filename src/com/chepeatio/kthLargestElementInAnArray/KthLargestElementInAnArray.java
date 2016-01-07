ece07ce5c3f1ab49dc26033be81016521a57e50c
chepeatio <chepeatio@hotmail.com>
2016-01-07 22:41:09 +0800

    找到了堆排序的算法，算法应该比较稳定，因此效率比较高，还需要再理解。

--- src/com/chepeatio/kthLargestElementInAnArray/KthLargestElementInAnArray.java
+++ src/com/chepeatio/kthLargestElementInAnArray/KthLargestElementInAnArray.java
@@ -4,7 +4,43 @@ package com.chepeatio.kthLargestElementInAnArray;
  * Created by Che Peatio on 2016/1/7.
  */
 public class KthLargestElementInAnArray {
+
     public int findKthLargest(int[] nums, int k) {
+        for(int i=nums.length/2; i>0; i--)
+            maxHeapify(nums, i, nums.length);
+
+        for(int i=1; i<k; i++) {
+            int tmp = nums[nums.length-i];
+            nums[nums.length-i] = nums[0];
+            nums[0] = tmp;
+
+            maxHeapify(nums, 1, nums.length-i);
+        }
+        return nums[0];
+    }
+
+    void maxHeapify(int[] nums, int index, int size) {
+        int left = index<<1, right = (index<<1)+1;
+        int max = index;
+
+        if(left<=size && nums[left-1]>nums[max-1]) max = left;
+        if(right<=size && nums[right-1]>nums[max-1]) max = right;
+        if(max!= index) {
+            int tmp = nums[max-1];
+            nums[max-1] = nums[index-1];
+            nums[index-1] = tmp;
+
+            maxHeapify(nums, max, size);
+        }
+    }
+
+    /**
+     * Quick select solution! Binary divide solution is very good, but bad efficient.
+     * @param nums list
+     * @param k target
+     * @return value
+     */
+    public int findKthLargestBES(int[] nums, int k) {
         return quickselect(nums, nums.length - k, 0, nums.length - 1);
     }
 
